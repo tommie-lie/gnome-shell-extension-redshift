2c76cee762dc76f3617c593b27947f67dc106a7d
Thomas Liebetraut <thomas@tommie-lie.de>
2013-05-27 18:09:08 +0200

    Start redshift with default settings and kill it again on switch toggle

--- src/extension.js
+++ src/extension.js
@@ -2,6 +2,8 @@
 const Lang = imports.lang;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
+const GLib = imports.gi.GLib;
+const Util = imports.misc.util;
 
 
 const RedshiftToggle = new Lang.Class({
@@ -9,7 +11,6 @@ const RedshiftToggle = new Lang.Class({
 
     _init: function() {
     },
-    
     enable: function() {
         let userMenu = Main.panel.statusArea.userMenu;
         // try to find the correct position:
@@ -17,10 +18,35 @@ const RedshiftToggle = new Lang.Class({
         let index = userMenu.menu._getMenuItems().indexOf(userMenu._notificationsSwitch);
         
         this.menuItem = new PopupMenu.PopupSwitchMenuItem("Redshift", false);
+        this.menuItem.connect("toggled", Lang.bind(this, this._toggle));
         userMenu.menu.addMenuItem(this.menuItem, index + 1);
     },
     disable: function() {
         this.menuItem.destroy();
+    },
+    
+    _toggle: function(item, state) {
+        if (state) {
+            let [success, pid] = GLib.spawn_async(null, ["redshift"], null,
+                             GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+                             null, null);
+            GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, Lang.bind(this, this._redshift_terminated), null);
+            this.pid = pid;
+        } else {
+            if (this.pid) {
+                Util.spawnCommandLine("kill -TERM " + this.pid);
+                // killing takes a few seconds due to the soft transition,
+                // we don't want to spawn in this time again, so disable
+                // the switch
+                this.menuItem.setSensitive(false);
+            }
+        }
+    },
+    _redshift_terminated: function(pid, status, user_data) {
+        GLib.spawn_close_pid(pid);
+        this.pid = null;
+        // reenable the switch again
+        this.menuItem.setSensitive(true);
     }
 });
 
