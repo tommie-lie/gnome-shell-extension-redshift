f85066b74bf48105ae138ec7f5cfde315e2ebccd
Thomas Liebetraut <thomas@tommie-lie.de>
2013-05-27 19:05:48 +0200

    Remember active state across sessions

--- src/extension.js
+++ src/extension.js
@@ -4,12 +4,15 @@ const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
 const GLib = imports.gi.GLib;
 const Util = imports.misc.util;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const RedshiftUtil = Me.imports.util;
 
 
 const RedshiftToggle = new Lang.Class({
     Name: "redshift",
 
     _init: function() {
+        this._settings = RedshiftUtil.getSettings();
     },
     enable: function() {
         let userMenu = Main.panel.statusArea.userMenu;
@@ -20,17 +23,26 @@ const RedshiftToggle = new Lang.Class({
         this.menuItem = new PopupMenu.PopupSwitchMenuItem("Redshift", false);
         this.menuItem.connect("toggled", Lang.bind(this, this._toggle));
         userMenu.menu.addMenuItem(this.menuItem, index + 1);
+        
+        this._settings.connect("changed::active", Lang.bind(this, this._enabledChanged));
+        // use stored value to initialize the extension
+        this._enabledChanged(this._settings, "active");
     },
     disable: function() {
         this.menuItem.destroy();
     },
     
     _toggle: function(item, state) {
+        this._settings.set_boolean("active", state);
+    },
+    _enabledChanged: function (settings, key) {
+        let state = settings.get_boolean(key);
+        this.menuItem.setToggleState(state);
         if (state) {
             let [success, pid] = GLib.spawn_async(null, ["redshift"], null,
                              GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD,
                              null, null);
-            GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, Lang.bind(this, this._redshift_terminated), null);
+            GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, Lang.bind(this, this._redshiftTerminated), null);
             this.pid = pid;
         } else {
             if (this.pid) {
@@ -42,7 +54,7 @@ const RedshiftToggle = new Lang.Class({
             }
         }
     },
-    _redshift_terminated: function(pid, status, user_data) {
+    _redshiftTerminated: function(pid, status, user_data) {
         GLib.spawn_close_pid(pid);
         this.pid = null;
         // reenable the switch again
